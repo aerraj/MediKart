9aaed7cb2227d5aa345e6eef99d87f448d3d3db7
aerraj <61013804+aerraj@users.noreply.github.com>
2024-05-26 18:20:09 +0530

    imp-cmp-dmp

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -17,9 +17,17 @@ export default function Navbar() {
     <div>
           <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
               <Link className="navbar-brand fs-1 font-weight-bold company mt-0 mb-0 m-4 glow-on-hover" to="/"> MediKart</Link>
-              <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="/navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                  <span className="navbar-toggler-icon"></span>
-              </button>
+              <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
               <div className="collapse navbar-collapse" id="navbarNav">
                   <ul className="navbar-nav me-auto mb-2">
                       <li className="nav-item">
