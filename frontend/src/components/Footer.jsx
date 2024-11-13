96683c8e1c8674930e14a3bb3f624a3ba9ed6a24
Rishabh Raj <rraj09102002@gmail.com>
2024-11-13 11:27:49 +0000

    initial-commit

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <div>
         <footer className="d-flex flex-wrap justify-content-center align-items-center py-3 my-4 border-top">
             <div className="col-12 d-flex justify-content-center align-items-center">
-              Designed and Built wirh ❤️in India
+              Designed and Built with ❤️ in India
                 <Link to="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
                     <span style={{ fontSize: '1.25rem' }}>© 2024 MediKart, Inc</span>
                 </Link>
